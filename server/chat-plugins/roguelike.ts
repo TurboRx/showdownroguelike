96836157d2bd64f8c70d6d516eb4155d2ede9fe8
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-03-26 13:01:00 -0400

    remove alola-totem pokemon

--- server/chat-plugins/roguelike.ts
+++ server/chat-plugins/roguelike.ts
@@ -198,7 +198,7 @@ function genPokemon(quantity: number, level: number | number[], starter?: boolea
 	const validate = new TeamValidator('gen9roguelikebattle');
 	const gennedMons: PokemonSet[] = [];
 
-	let all = Dex.species.all().filter(s => !s.battleOnly && !s.requiredItems && s.forme !== 'Gmax' && s.forme !== 'Eternamax' && s.forme !== 'Totem' && s.forme !== 'Dusk' && s.forme !== 'Bond' && !(s.isNonstandard && s.isNonstandard !== 'Past'));
+	let all = Dex.species.all().filter(s => !s.battleOnly && !s.requiredItems && s.forme !== 'Gmax' && s.forme !== 'Eternamax' && !s.forme.includes('Totem')  && s.forme !== 'Dusk' && s.forme !== 'Bond' && !(s.isNonstandard && s.isNonstandard !== 'Past'));
 	if (starter) {
 		all = all.filter(s => !s.prevo);
 
