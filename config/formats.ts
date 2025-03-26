57d61eef0970559ad4d30a534921313a3a3a269a
HiZo <96159984+HisuianZoroark@users.noreply.github.com>
2025-03-26 18:54:39 -0400

    Keep HP proportions when levelling up

--- config/formats.ts
+++ config/formats.ts
@@ -4424,10 +4424,12 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 						source.level++;
 						source.set.level++;
 						if (source.baseSpecies.name !== 'Shedinja') {
+							let percent = source.hp / source.baseMaxhp;
 							source.baseMaxhp = Math.floor(Math.floor(
 								2 * source.species.baseStats['hp'] + source.set.ivs['hp'] + Math.floor(source.set.evs['hp'] / 4) + 100
 							) * source.level / 100 + 10);
 							source.maxhp = source.baseMaxhp;
+							source.hp = Math.floor(source.baseMaxhp * percent);
 						}
 						source.details = source.getUpdatedDetails();
 						this.add('detailschange', source, source.details);
